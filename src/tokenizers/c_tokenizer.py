e410cc7e5200abe0f764bf05061de045fe68f94d
Олег Есиков <esikov.oleg@mail.ru>
2021-01-04 15:54:47 +0300

    Update c-tokenizer - add new constructions

--- src/tokenizers/c_tokenizer.py
+++ src/tokenizers/c_tokenizer.py
@@ -26,18 +26,26 @@ class CTokenizer(Tokenizer):
     # R - Return - возврат значения из функции
     # I - If - условные конструкции
     # S - Series - циклы
+    # E - сравнения
+    # L - Logic - логические операции
+    # U - Upheaval - побитовые операции сдвига
+    # G - Governance - управляющие конструкции
     def _process(self, src):
         print("tokenize c-file\n")
         change_code = self.clear_space(src)
         print(change_code, "\n")
         # Замена названий переменных, которые совпадают с именами токенов
-        change_code = re.sub(r'[NDBPCAFTMRIS]', "X", change_code)
+        change_code = re.sub(r'[NDBPCAFTMRISELUG]', "X", change_code)
         # Токенизация возврата из функции
         change_code = re.sub(r'return', "$R$", change_code)
-        # Токенизация определеня функции
-        change_code = re.sub(r'([a-zA-Z_]\w*)\(([a-zA-Z_]\w*,*)*\){', "$F${", change_code)
+        # Токенизация указателей на функцию
+        change_code = re.sub(r'\w+\*?\(\*[\w\[\]]+\)\(([\w$.*\[\]]*,*)*\)', "$P$", change_code)
+        # Токенизация определения функции
+        change_code = re.sub(r'([a-zA-Z_][\w*]*)\(([\w$.*\[\]]*,*)*\){', "$F${", change_code)
+        # Токенизация функции, возвращающей указатель на функцию
+        change_code = re.sub(r'\w+\*?\(\*[\w\[\]]+\(([\w$.*\[\]]*,*)*\)\)\(([\w$.*\[\]]*,*)*\){', "$F${", change_code)
         # Токенизация вызова функции
-        change_code = re.sub(r'([a-zA-Z_]\w*)\([^;!=><|&]*\)(;|!=|,|==|>|<|>=|<=|&&|\|\|)', "$C$", change_code)
+        change_code = re.sub(r'([a-zA-Z_]\w*)\([^;!><|&]*\)(;|!=|,|==|>|<|>=|<=|&&|\|\|)', "$C$", change_code)
         # Токенизация приведения типа
         change_code = re.sub(r'\([a-zA-Z_]\w*\**\)', "$T$", change_code)
         # Токенизация основных типов данных
@@ -49,23 +57,35 @@ class CTokenizer(Tokenizer):
         change_code = re.sub(r'({int_types})([a-zA-Z_]\w*;?)?'.format(int_types=self.__int_types), "$N$", change_code)
         print(change_code, "\n")
         # Токенизация циклов
+        change_code = re.sub(r'do', "$S$", change_code)
+        change_code = re.sub(r'while\([^;]*\);', "", change_code)
         change_code = re.sub(r'(for|while)\([^)]*\)', "$S$", change_code)
+        # Токенизация управляющих конструкций
+        change_code = re.sub(r'continue|break|goto', "$G$", change_code)
         # Токенизация условных конструкций
         change_code = re.sub(r'(((if|elseif)\([^)]*\))|else)', "$I$", change_code)
-        # Токенизация присваивания и математического выражения в виде одного оператора
+        # Токенизация сочетаний оператора присваивания
         change_code = re.sub(r'[+\-*/%]=', "$A$$M$", change_code)
+        change_code = re.sub(r'(<<|>>|&|\^|\|)=', "$A$$U$", change_code)
         # Токенизация математических выражений
         change_code = re.sub(r'(([a-zA-Z_]\w*)\+\+)|(\+\+[a-zA-Z_]\w*)', "$M$", change_code)
         change_code = re.sub(r'(([a-zA-Z_]\w*)--)|(--[a-zA-Z_]\w*)', "$M$", change_code)
         change_code = re.sub(r'[^={]\*', "$M$", change_code)
-        change_code = re.sub(r'[+\-/%]', "$M$", change_code)
+        change_code = re.sub(r'[^={}><|&][+\-/%]', "$M$", change_code)
         print(change_code, "\n")
+        # Токенизация логических операций
+        change_code = re.sub(r'&&|\|\||!', "$L$", change_code)
+        # Токенизация побитовых операций
+        change_code = re.sub(r'[^={}><|&](<<|>>|&|\^|\|)', "$U$", change_code)
+        change_code = re.sub(r'~', "$U$", change_code)
+        # Токенизация сравнений
+        change_code = re.sub(r'==|>|<|<=|>=|!=', "$E$", change_code)
         # Токенизация присваивания
-        change_code = re.sub(r'=', "$A$", change_code)
+        change_code = re.sub(r'=(\{[^;]*};)?', "$A$", change_code)
         # Удаление всех символов не соответствующих токенам
         # change_code = re.sub(r'\([^)$]*\)', "$()$", change_code)
         print(change_code, "\n")
-        change_code = re.sub(r'[^{}NDBPCAFTMRIS]*', "", change_code)
+        change_code = re.sub(r'[^{}NDBPCAFTMRISELUG]*', "", change_code)
 
         return change_code
 
