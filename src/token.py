21fee76ba3d4596a920b6507f83fc28b26e49c17
Олег Есиков <esikov.oleg@mail.ru>
2021-01-24 14:09:57 +0300

    Add method for search border similarity src

--- src/token.py
+++ src/token.py
@@ -22,3 +22,14 @@ class Token:
         for token in sorted(token_list, key=lambda tok: tok.start):
             token_str += token.symbol
         return token_str
+
+    @staticmethod
+    def find_border_tokens_str_in_token_list(token_list, token_str):
+        for i in range(len(token_list)):
+            j = 0
+            while j < len(token_str) and i + j < len(token_list) and token_list[i + j].symbol == token_str[j]:
+                j += 1
+            if j == len(token_str):
+                return token_list[i].start, token_list[i + j - 1].end
+            i += 1
+        return None
