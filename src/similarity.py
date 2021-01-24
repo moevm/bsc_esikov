9e451337de97c6430c41baaad837a6a5aea9527a
Олег Есиков <esikov.oleg@mail.ru>
2021-01-24 14:58:19 +0300

    Add search similarity src

---
+++ src/similarity.py
@@ -0,0 +1,20 @@
+from src.token import Token
+
+
+class Similarity:
+    def __init__(self, check_file, detected_file, similarity_tokens_sequences):
+        self.__check_file = check_file
+        self.__detected_file = detected_file
+        self.__similarity_tokens_sequences = similarity_tokens_sequences
+
+    def get_similarity_src(self):
+        check_file_similarity_src = []
+        detected_file_similarity_src = []
+        for sim_tokens_str in self.__similarity_tokens_sequences:
+            check_file_similarity_src.append(self._get_str_src_from_token_str(self.__check_file, sim_tokens_str))
+            detected_file_similarity_src.append(self._get_str_src_from_token_str(self.__detected_file, sim_tokens_str))
+        return check_file_similarity_src, detected_file_similarity_src
+
+    def _get_str_src_from_token_str(self, src_file, sim_tokens_str):
+        start, end = Token.find_border_tokens_str_in_token_list(src_file.tokens, sim_tokens_str)
+        return src_file.src[start:end]
