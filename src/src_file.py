fc085d0359a066a0f3146e9a3c23488ef14246a1
Олег Есиков <esikov.oleg@mail.ru>
2021-01-24 13:05:52 +0300

    Add tokens property in SrcFile

--- src/src_file.py
+++ src/src_file.py
@@ -1,8 +1,12 @@
+from src.token import Token
+
+
 class SrcFile:
     def __init__(self, name, path, src):
         self.__name = name
         self.__path = path
         self.__src = src
+        self.__tokens = []
 
     @property
     def name(self):
@@ -15,3 +19,15 @@ class SrcFile:
     @property
     def src(self):
         return self.__src
+
+    @property
+    def tokens(self):
+        return self.__tokens
+
+    @tokens.setter
+    def tokens(self, tokens):
+        self.__tokens = tokens
+
+    @property
+    def tokens_str(self):
+        return Token.get_tokens_str_from_token_list(self.__tokens)
