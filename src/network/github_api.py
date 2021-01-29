d9bada1d6965dbf0207cf027c735cd959a1d6fcf
Олег Есиков <esikov.oleg@mail.ru>
2021-01-29 23:35:01 +0300

    Replace return request result

--- src/network/github_api.py
+++ src/network/github_api.py
@@ -18,23 +18,23 @@ class GithubAPI:
         headers = {
             'Authorization': 'token ' + self.__token
         }
-        return requests.get(address + api_url, headers=headers)
+        return requests.get(address + api_url, headers=headers).json()
 
     def get_name_default_branch(self, owner_login, repo_name):
         api_url = '/repos/{owner}/{repo}'.format(owner=owner_login, repo=repo_name)
-        response = self.__send_request(api_url)
-        return response.json()['default_branch']
+        response_json = self.__send_request(api_url)
+        return response_json['default_branch']
 
     def get_sha_last_commit_in_default_branch(self, owner_login, repo_name, branch_name):
         api_url = '/repos/{owner}/{repo}/branches/{branch}'.format(owner=owner_login, repo=repo_name, branch=branch_name)
-        response = self.__send_request(api_url)
-        return response.json()['commit']['sha']
+        response_json = self.__send_request(api_url)
+        return response_json['commit']['sha']
 
     def get_src_file_from_sha(self, owner_login, repo_name, sha, file_path):
         api_url = '/repos/{owner}/{repo}/git/blobs/{sha}'.format(owner=owner_login, repo=repo_name, sha=sha)
-        response = self.__send_request(api_url)
-        if response.json()['encoding'] == 'base64':
-            file_bytes = base64.b64decode(response.json()['content'])
+        response_json = self.__send_request(api_url)
+        if response_json['encoding'] == 'base64':
+            file_bytes = base64.b64decode(response_json['content'])
             src = file_bytes.decode('utf-8')
             return SrcFile(os.path.basename(file_path), file_path, src)
         else:
@@ -43,8 +43,8 @@ class GithubAPI:
 
     def get_files_generator_from_sha_commit(self, owner_login, repo_name, sha, file_path='.'):
         api_url = '/repos/{owner}/{repo}/git/trees/{sha}'.format(owner=owner_login, repo=repo_name, sha=sha)
-        response = self.__send_request(api_url)
-        tree = response.json()['tree']
+        response_json = self.__send_request(api_url)
+        tree = response_json['tree']
         for node in tree:
             current_path = file_path + "/" + node["path"]
             if node["type"] == "tree":
