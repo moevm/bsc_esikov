e721a3282605694c08e63b34e4b251b126ea19f4
Олег Есиков <esikov.oleg@mail.ru>
2021-01-30 13:27:35 +0300

    Big update main. Add search in github repo and search by url file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from settings import settings
 from src import argv_parser
 from src.tokenizers.c_tokenizer import CTokenizer
 from src.console.dir_scanner import DirScanner
@@ -7,66 +8,62 @@ from src.algorithms.greedy_string_tiling import GreedyStringTiling
 from src.token import Token
 from src.similarity import Similarity
 from src.src_file import SrcFile
+from src.network.github_api import GithubAPI
+from src.network.url_parser import UrlParser
+
+
+def get_search_file():
+    if UrlParser.is_url(SEARCH_FILE_PATH):
+        return GITHUB_API.get_file_from_url(SEARCH_FILE_PATH)
+    else:
+        return DirScanner.read_file(SEARCH_FILE_PATH)
+
+
+def scan_dir(comparable_file):
+    if UrlParser.is_url(SEARCH_DIR):
+        dir_scanner = GITHUB_API.get_files_generator_from_repo_url
+    else:
+        dir_scanner = SCANNER.scan
+
+    for file in dir_scanner(SEARCH_DIR):
+        similarity_percentage = HESKEL_ALGO.search(TOKENIZER.fast_tokenize(file.src))
+        if similarity_percentage > LIMIT and comparable_file.path != file.path:
+            file.similarity_percentage = similarity_percentage
+            yield file
+
+
+def get_similarity(comparable_file):
+    for file in scan_dir(comparable_file):
+        file.tokens = TOKENIZER.tokenize(file.src)
+        similarity_tokens_sequence = GREEDY_ALGO.search(file.tokens_str)
+        yield Similarity(comparable_file, file, similarity_tokens_sequence)
 
 
 if __name__ == "__main__":
     parameters = argv_parser.parse()
 
+    FILE_EXTENSION = "c"
     SEARCH_FILE_PATH = parameters.file
+    if not SrcFile.is_file_have_this_extension(SEARCH_FILE_PATH, FILE_EXTENSION):
+        print('Приложение поддерживает только файлы с расширением .c')
+        sys.exit(-1)
     SEARCH_DIR = parameters.dir
     try:
         LIMIT = int(parameters.limit)
     except ValueError as e:
         print("Введённое предельное значение не является числом: " + parameters.limit)
         sys.exit(-1)
-    FILE_EXTENSION = "c"
+    GITHUB_API = GithubAPI(settings['GITHUB_TOKEN'], FILE_EXTENSION)
+    SCANNER = DirScanner(FILE_EXTENSION)
+    TOKENIZER = CTokenizer()
 
-    try:
-        with open(SEARCH_FILE_PATH, "r", encoding="utf-8") as srcFile:
-            search_file = SrcFile(SEARCH_FILE_PATH, SEARCH_FILE_PATH, srcFile.read())
-    except FileNotFoundError as e:
-        print("Введённый файл не найден: " + SEARCH_FILE_PATH)
-        sys.exit(-1)
-    except UnicodeDecodeError as e:
-        print("Файл " + SEARCH_FILE_PATH + " не удалось прочитать - не в кодировке utf-8")
-        sys.exit(-1)
-    except OSError as e:
-        print("Введённый параметр не является файлом: " + SEARCH_FILE_PATH)
-        sys.exit(-1)
+    search_file = get_search_file()
+    HESKEL_ALGO = Heskel(TOKENIZER.fast_tokenize(search_file.src))
+    search_file.tokens = TOKENIZER.tokenize(search_file.src)
+    GREEDY_ALGO = GreedyStringTiling(Token.get_tokens_str_from_token_list(search_file.tokens))
 
-    tokenizer = CTokenizer()
-    scanner = DirScanner(FILE_EXTENSION)
-
-    search_file_tokens = tokenizer.tokenize(search_file.src)
-    search_file.tokens = search_file_tokens
-
-    heskel_algo = Heskel(tokenizer.fast_tokenize(search_file.src))
-    greedy_algo = GreedyStringTiling(Token.get_tokens_str_from_token_list(search_file_tokens))
-
-    files = scanner.scan(SEARCH_DIR)
-    similarity_files = []
-    print("Выполняется подсчёт процентов совпадений")
-    for file in files:
-        similarity_percentage = heskel_algo.search(tokenizer.fast_tokenize(file.src))
-        if similarity_percentage > LIMIT and search_file.path != file.path:
-            file.similarity_percentage = similarity_percentage
-            similarity_files.append(file)
-
-    similarity = []
-    print("Выполняется поиск схожих фрагментов")
-    for file in similarity_files:
-        tokens = tokenizer.tokenize(file.src)
-        file.tokens = tokens
-        tokens_str = file.tokens_str
-        similarity_tokens_sequence = greedy_algo.search(tokens_str)
-        similarity.append(Similarity(search_file, file, similarity_tokens_sequence))
-
-    if len(similarity) > 0:
-        print("Полученный результат:")
-    else:
-        print("В директории не было обнаружено файлов с процентом совпадения > " + str(LIMIT))
-
-    for sim in similarity:
+    is_find_similarity = False
+    for sim in get_similarity(search_file):
         check_file_similarity_src, detected_file_similarity_src = sim.get_similarity_src()
         print("*" * 60)
         print(sim.check_file_path + ":")
@@ -80,3 +77,6 @@ if __name__ == "__main__":
             print(src)
             print("-" * 60)
         print("\n")
+        is_find_similarity = True
+    if is_find_similarity is False:
+        print("В директории не было обнаружено файлов с процентом совпадения > " + str(LIMIT))
