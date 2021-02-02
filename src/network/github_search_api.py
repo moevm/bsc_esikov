bb631e7578b4c1e7afdc2a825cfa6d38e06741c3
Олег Есиков <esikov.oleg@mail.ru>
2021-02-02 17:18:09 +0300

    Add search code in github

---
+++ src/network/github_search_api.py
@@ -0,0 +1,49 @@
+import requests
+from src.network.search_api import SearchAPI
+from src.network.github_api import GithubAPI
+
+
+class GithubSearchAPI(SearchAPI):
+    def __init__(self, file_extension, token):
+        SearchAPI.__init__(self, file_extension)
+        self._token = token
+
+    @property
+    def languages(self):
+        return {
+            "c": "c",
+            "java": "java",
+            "py": "python"
+        }
+
+    def _send_search_request(self, func_name, page, per_page=50):
+        url = 'https://api.github.com/search/code'
+        headers = {
+            'Authorization': 'token ' + self._token,
+            'accept': 'application/vnd.github.v3+json',
+        }
+        q = '?q={func_name}+in:file+language:{language}&page={page}&per_page={per_page}'.format(
+            func_name=func_name,
+            language=self.languages[self._file_extension],
+            page=page,
+            per_page=per_page
+        )
+        response = requests.get(url + q, headers=headers, timeout=7)
+        response.raise_for_status()
+        return response
+
+    def search(self, func_name):
+        github = GithubAPI(self._token, self._file_extension)
+        page = 1
+        while True:
+            response_json = self._send_search_request(func_name, page).json()
+            if response_json['items'] is None or len(response_json['items']) == 0:
+                break
+            for item in response_json['items']:
+                owner_login = item["repository"]["owner"]["login"]
+                repo_name = item["repository"]["name"]
+                path = "./" + item["path"]
+                file = github.get_src_file_from_sha(owner_login, repo_name, item['sha'], path)
+                file.source = item["repository"]["html_url"]
+                yield file
+            page += 1
