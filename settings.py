7a655607bfb38e12f359e0abb1f94f3cf7875f4d
Олег Есиков <esikov.oleg@mail.ru>
2021-02-07 20:20:17 +0300

    Add check exists env var

--- settings.py
+++ settings.py
@@ -5,11 +5,15 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
 if not os.path.exists(dotenv_path):
-    print("Файл .env с параметрами не существует в корневой директории")
+    print("Файл .env не существует в корневой директории")
     sys.exit(-1)
 
 load_dotenv(dotenv_path)
 
-settings = {
-    'GITHUB_TOKEN': os.getenv('GITHUB_TOKEN'),
-}
+try:
+    settings = {
+        'GITHUB_TOKEN': os.environ['GITHUB_TOKEN'],
+    }
+except KeyError as e:
+    print("Файл .env не содержит переменную окружения " + str(e)[1:-1])
+    sys.exit(-1)
