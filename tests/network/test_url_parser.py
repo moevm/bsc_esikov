9a66d70c6a518b5ecfb0230d3dedf6598d0f23ff
Олег Есиков <esikov.oleg@mail.ru>
2021-02-13 17:27:22 +0300

    Fix test url parser

--- tests/network/test_url_parser.py
+++ tests/network/test_url_parser.py
@@ -16,30 +16,34 @@ class TestUrlParser(unittest.TestCase):
 
     def test_parse_github_file_path(self):
         url = 'https://github.com/microsoft/TypeScript/blob/master/.gitattributes'
-        owner, repo, branch, path = UrlParser.parse_github_file_path(url)
+        owner, repo, branch, path = UrlParser.parse_github_content_path(url)
         self.assertEqual(owner, 'microsoft')
         self.assertEqual(repo, 'TypeScript')
         self.assertEqual(branch, 'master')
         self.assertEqual(path, '.gitattributes')
 
         url = 'https://github.com/microsoft/TypeScript/blob/master/src/webServer/webServer.ts'
-        owner, repo, branch, path = UrlParser.parse_github_file_path(url)
+        owner, repo, branch, path = UrlParser.parse_github_content_path(url)
         self.assertEqual(owner, 'microsoft')
         self.assertEqual(repo, 'TypeScript')
         self.assertEqual(branch, 'master')
         self.assertEqual(path, 'src/webServer/webServer.ts')
 
         url = 'https://github.com/microsoft/TypeScript/blob/configBuild/scripts/authors.ts'
-        owner, repo, branch, path = UrlParser.parse_github_file_path(url)
+        owner, repo, branch, path = UrlParser.parse_github_content_path(url)
         self.assertEqual(owner, 'microsoft')
         self.assertEqual(repo, 'TypeScript')
         self.assertEqual(branch, 'configBuild')
         self.assertEqual(path, 'scripts/authors.ts')
 
         url = 'https://github.com/microsoft/TypeScript/tree/master/doc'
-        self.assertRaises(ValueError, UrlParser.parse_github_file_path, url)
+        owner, repo, branch, path = UrlParser.parse_github_content_path(url)
+        self.assertEqual(owner, 'microsoft')
+        self.assertEqual(repo, 'TypeScript')
+        self.assertEqual(branch, 'master')
+        self.assertEqual(path, 'doc')
 
-        self.assertRaises(ValueError, UrlParser.parse_github_file_path, 'https://www.google.com/')
+        self.assertRaises(ValueError, UrlParser.parse_github_content_path, 'https://www.google.com/')
 
     def test_is_url(self):
         self.assertTrue(UrlParser.is_url('https://github.com'))
